4d260b11df6b4d7a39d32ffbe7a498d3865ab3f8
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2025-05-16 14:29:39 +0700

    Replacement 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import ReactDOM from 'react-dom/client'
-import {Theme} from "@radix-ui/themes"
-import './index.css'
-import App from './app.tsx'
+import App from '@/app.tsx'
 
 const root_element = document.getElementById('root');
 
 if (root_element != null){
   const root = ReactDOM.createRoot(root_element);
   root.render(
-    <Theme>
-      <div className={"bg-white px-6 py-2 text-[clamp(3.5rem, 12vw + 1rem, 12rem)]"}>
-        <App/>
-      </div>
-    </Theme>
+    <div className={"bg-white px-6 py-2 text-[clamp(3.5rem, 12vw + 1rem, 12rem)]"}>
+      <App/>
+    </div>
   )
 }
